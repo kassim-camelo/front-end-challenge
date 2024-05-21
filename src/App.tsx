7ee8f107d5de45f7ce858bfc7756f2c14770f370
Thiago Camelo <thiago.la1707@gmail.com>
2024-05-20 22:19:33 -0300

    bugfix: scrollbar aparecendo mesmo quando não era para aparecer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { Box, CssBaseline } from "@mui/material"
+import { Box, CssBaseline, styled } from "@mui/material"
 
 import Banner from "./components/banner/banner"
 import Home from "./components/home/home"
 import Library from "./components/library/library"
 import Sidebar from "./components/sidebar/sidebar"
 
+const StyledBox = styled(Box)`
+  overflow: hidden;
+  background-color: black;
+  max-height: 100vh;
+`
+
 function App() {
   return (
-      <Box sx={{ bgcolor: 'black', maxHeight: '100vh', minHeight: '100vh' }}>
+      <StyledBox>
         <CssBaseline />
         <Box sx={{ display: 'flex' }}>
           <Box sx={{ maxWidth: '450px' }}>
@@ -19,7 +25,7 @@ function App() {
           </Box>
         </Box>
         <Banner />
-      </Box>
+      </StyledBox>
   )
 }
 
